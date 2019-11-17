76890a07f30d69fce83e00b8b75789331279aebf
Julie Engel <julie.engel@mail.com>
2019-11-17 11:32:47 +0100

    Rename exam to msp, re-structure controller and static site.

--- grails-app/controllers/mvc/CalculatorController.groovy
+++ grails-app/controllers/mvc/CalculatorController.groovy
@@ -1,9 +1,8 @@
 package mvc
 
 class CalculatorController {
-
-    def calc(double en, double exam) {
-        double result = (en + exam) / 2
+    def calc(double en, double msp) {
+        double result = (en + msp) / 2
         render view: "CalculatorOutput", model: [result: result]
     }
 }
